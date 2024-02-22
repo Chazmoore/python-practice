973f2623369a343f84e2bf367b44de00088f9790
LaChaznie Moore <lachazniemoore@LaChaznies-MacBook-Air.local>
2024-02-21 23:19:24 -0500

    started ch. 7

---
+++ userinput_ch7/greeter.py
@@ -0,0 +1,18 @@
+# name = input("Please enter your name:")
+# print(f"\nHello, {name}!")
+
+# Prompt = "If you share your name, we can personalize the messages you see."
+# Prompt += "\nWhat is your name?"
+
+# name = input(Prompt)
+# print(f"\nHello, {name}!")
+
+# age = input("How old are you? ")
+# print(f'\nYour Age is {age}.')
+
+age = input("How old are you? ")
+print(f"\nYour Age is {age}.")
+
+age = int(age)
+age >= 18
+print(True)
---
+++ userinput_ch7/rollercoaster.py
@@ -0,0 +1,8 @@
+height = input("How tall are you, in inches?")
+height = int(height)
+
+if height >= 48:
+    print("\nYou're Tall enough to ride!")
+
+else:
+    print("\nYou're to short for this ride.")
